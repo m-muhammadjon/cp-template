48ae05c08a568042f49ddfa260ae652a6371cb1c
Muhamedov Muhammadjon <84259873+m-muhammadjon@users.noreply.github.com>
2023-10-08 09:52:39 +0500

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define vstr vector<string>
+#define vchar vector<char>
+#define vbool vector<bool>
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)x.size()
+#define rep(i, a, b) for(int i=a;i<b;i++)
+#define rrep(i, a, b) for(int i=a;i>=b;i--)
+
+const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
+const ll INF = 1e18 + 7;
+const int N = 1e5 + 7;
+const int M = 1e5 + 7;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {1, 1, 1, 0, 0, -1, -1, -1};
+const int ddy[8] = {1, 0, -1, 1, -1, 1, 0, -1};
+
+void solve() {
+    cout << "Hello World!" << endl;
+}
+
+int main() {
+    	// ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+//    int t;
+//    cin >> t;
+//    while (t--) {
+//        solve();
+//    }
+    return 0;
+}
